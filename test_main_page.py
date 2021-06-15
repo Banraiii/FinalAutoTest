ccb99012be480ddf406c2fd6054086c64918c958
unknown <arhersleep@gmail.com>
2021-06-15 16:39:03 +0300

    Не желательное решение с setup и teardown методами

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,23 @@
 from .pages.product_page import ProductPage
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import time
 import pytest
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+	def test_guest_can_go_to_login_page(self, driver):
+		link = "http://selenium1py.pythonanywhere.com"
+		page = MainPage(driver, link)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+		page.open()						# открываем страницу
+		page.should_be_login_link()		# выполняем проверку нахождения логин линк
+		page.go_to_login_page()			# выполняем метод страницы — переходим на страницу логина
+		login_page = LoginPage(driver, driver.current_url)
+		login_page.should_be_login_page()
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(driver):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/"
-	page = MainPage(driver, link)
-	page.open()
-	page.should_be_basket_button()
-	page.go_to_the_basket_in_havbar()
-	basket_page = BasketPage(driver, driver.current_url)
-	time.sleep(2)
-	basket_page.should_not_be_item_in_basket()
-	basket_page.should_be_message_on_empty_bucket()
+	def test_guest_should_see_login_link(self, driver):
+		link = "http://selenium1py.pythonanywhere.com"
+		page = MainPage(driver, link)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+		page.open()						# открываем страницу
+		page.should_be_login_link()		# выполняем проверку нахождения логин линк
--- test_product_page.py
+++ test_product_page.py
@@ -1,16 +1,31 @@
 from .pages.product_page import ProductPage
-#from .pages.main_page import MainPage
+from .pages.main_page import MainPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 import time
 import pytest
 
 
-def test_guest_cant_see_product_in_basket_opened_from_product_page(driver):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
-	page = ProductPage(driver, link)
-	page.open()
-	page.should_be_basket_button()
-	page.go_to_the_basket_in_havbar()
-	basket_page = BasketPage(driver, driver.current_url)
-	basket_page.should_not_be_item_in_basket()
-	basket_page.should_be_message_on_empty_bucket()
+class TestUserAddToBasketFromProductPage():
+	@pytest.fixture(scope="function", autouse=True)
+	def setup(self, driver):
+		link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+		login_page = LoginPage(driver, link)
+		login_page.open()
+		email = str(time.time()) + "@fakemail.org"
+		login_page.register_new_user(email, 'QWer3213ty123')
+		login_page.should_be_authorized_user()
+
+	def test_user_can_add_product_to_basket(self, driver):
+		link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+		product_page = ProductPage(driver, link)
+		product_page.open()
+		product_page.shold_be_add_button()
+		product_page.add_to_card()
+		product_page.shold_be_message_add()
+
+	def test_user_cant_see_success_message(self, driver):
+		link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+		product_page = ProductPage(driver, link)
+		product_page.open()
+		product_page.shold_not_be_message_in_page()
